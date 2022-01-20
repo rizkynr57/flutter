305a42ef762fdad0d4c617707f14add62e561046
rizkynr57 <rizky.nurahman48@gmail.com>
2022-01-20 21:52:13 +0500

    pkl

---
+++ looping/latihan.dart
@@ -0,0 +1,16 @@
+void main() {
+    print("\n");
+    print("Looping Pertama");
+    var nilai = 2;
+    while (nilai <= 20) {
+        print(nilai.toString() + " - I love coding");
+        nilai+=2;
+    }
+
+    print("Looping Kedua");
+    var nilai2 = 20;
+    while (nilai2 >= 2) {
+        print(nilai2.toString() + " - I will become a mobile developer");
+        nilai2-=2;
+    }
+}
