ab1e2785a56d3cf01e8e3f0e902df328a23804e7
α <rizky.nurahman48@gmail.com>
2022-01-21 14:52:01 +0700

    Update latihan3.dart

---
+++ looping/latihan3.dart
@@ -0,0 +1,11 @@
+void main(){
+    for (var a = 4; a > 0; a--){
+        for (var b = 8; b >= a; b--){
+            print("#");
+        }
+        for (var c = 2; c <= a; c++){
+            print("#");
+        }
+        print("\n");
+    }
+}
